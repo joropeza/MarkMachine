dd35fb5b52e115ec63119e16b795040004913b08
joropeza <jon@zacsi.com>
2013-07-17 19:47:05 -0700

    Added Candle Class

---
+++ MarkMachine/Models/Candle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarkMachine.Models
+{
+    public class Candle
+    {
+        public DateTime CandleStart { get; set; }
+        public DateTime CandleEnd { get; set; }
+        public string Period { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+        
+    }
+}
