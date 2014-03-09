92295c8be01ee3664c3f788df3f51b02c002b9ef
joropeza <jon@zacsi.com>
2014-03-09 10:38:20 -0700

    Raw Data into Candles (daily candles)
    Next steps involve marking day features (gaps, etc)

---
+++ MarketMachineConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarketMachineProcedures;
+
+namespace MarketMachineConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessRunner pr = new ProcessRunner();
+            pr.CandleCatchupAll();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MarketMachineProcedures/ProcessRunner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketMachineProcedures
+{
+    public class ProcessRunner
+    {
+
+        public void CandleCatchupAll()
+        {
+            //grab the list of currently active markets
+            List<int> MarketList = new List<int>();
+            MarketList.Add(2);
+
+            foreach (var m in MarketList)
+            {
+                CandleCatchupMarket(m);
+
+            }
+            
+
+
+        }
+
+
+        public void CandleCatchupMarket(int MarketId)
+        {
+            CandleProcesses cp = new CandleProcesses();
+
+            //make sure each period's candles have been entered
+
+            cp.CatchUpDailyCandles(2);            
+
+            //catch up the current W and M candles
+
+
+
+
+
+        }
+    }
+}
