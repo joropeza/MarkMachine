3a644c8962c041e3552387ea4072ac5103aed7a1
joropeza <jon@zacsi.com>
2014-05-08 11:31:23 -0700

    stuff

---
+++ MarketMachineCore/Services/MarketsService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketMachineCore.Services
+{
+    public abstract class ICoreService
+    {
+        public IServiceResponseMessage request;
+    }
+
+    public class MarketsService : ICoreService
+    {
+
+        public IServiceResponseMessage List(ServiceResponseMessage<int> newRequest)
+        {
+            newRequest.RequestObject = 5;
+
+            this.request = newRequest;
+                     
+            return request;
+
+        }
+
+    }
+}
---
+++ MarketMachineCore/Services/ServiceResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketMachineCore.Services
+{
+    public interface IServiceArgument
+    {
+        object ArgumentValue { get; set; }
+    }
+
+    public class ServiceArgument<T> : IServiceArgument
+    {
+        object IServiceArgument.ArgumentValue
+        {
+            get
+            {
+                return this.Value;
+            }
+
+            set
+            {
+                this.Value = (T)value;
+            }
+        }
+
+        public T Value { get; set; }
+
+        public ServiceArgument(object newValue)
+        {
+
+        }
+
+    }
+
+    public interface IServiceResponseMessage
+    {
+
+    }
+
+    public class ServiceResponseMessage<T> : IServiceResponseMessage
+    {
+        public T RequestObject { get; set; }
+        public Exception Exception { get; set; }
+        public int ElapsedTime { get; set; }
+        public IServiceArgument Argument { get; set; }
+    }
+}
